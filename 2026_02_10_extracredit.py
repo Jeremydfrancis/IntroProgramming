6ed6d4a2f7b7ac3cf9881527f36503ed97a93ef2
Jeremy Francis <126933820+Jeremydfrancis@users.noreply.github.com>
2026-03-22 22:19:20 -0400

    Delete 2026_02_10_extracredit.py

--- 2026_02_10_extracredit.py
+++
@@ -1,8 +0,0 @@
-"""
-Jeremy Francis
-2026_02_10_extracredit
-Creating Half Pyramid
-"""
-rows = int(input("\nHow many rows for the triangle? "))
-for i in range (1, rows + 1):
-            print("#"*i)
